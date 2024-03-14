a99a8204054cb63827958ede2b4ab609e4e50462
Leen Shaikh Ibrahim <106743782+LeenShaikh@users.noreply.github.com>
2024-03-14 10:55:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const userRoute = require('./routes/userRoute');
 const projectRoute = require('./routes/projectRoute');
+const collaborationRoute = require('./routes/collaborationRoute');
+const resourceRoute = require('./routes/resourceRoute');
 const app = express();
 
 const uri = "mongodb://localhost:27017/?appName=mongodb-vscode&directConnection=true&serverSelectionTimeoutMS=2000";
@@ -28,6 +30,9 @@ app.use((req, res, next) => {
 
 app.use('/user', userRoute);
 app.use('/project',projectRoute);
+app.use('/collaborations', collaborationRoute);
+app.use('/resources', resourceRoute);
+
 app.listen(5000, () => {
     console.log(`Server running at http://localhost:5000`);
 });
